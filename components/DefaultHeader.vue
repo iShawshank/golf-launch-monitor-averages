3223371f7a45964d81c18346ba7072db2335ad86
Kevin Shaw <ishawshank39@gmail.com>
2023-03-19 21:41:51 -0600

    update header links on mobile

--- components/DefaultHeader.vue
+++ components/DefaultHeader.vue
@@ -1,10 +1,10 @@
 <template>
-  <header class="header flex px-10 py-1 bg-light-black border-neutral-2 sticky top-0">
-    <nav class="header__links">
+  <header class="header px-10 py-1 bg-light-black border-neutral-2 sticky top-0 h-15 md:h-10">
+    <nav class="header__links grid grid-cols-3 md:grid-cols-6">
       <NuxtLink class="header__link border-r-2" to="/" >Home</NuxtLink>
       <NuxtLink class="header__link border-r-2" to="/PGA" >PGA</NuxtLink>
-      <NuxtLink class="header__link border-r-2" to="/LPGA" >LPGA</NuxtLink>
-      <NuxtLink class="header__link" to="https://account.venmo.com/u/ishawshank39" target="_blank" >If you're enjoying this app, buy me a 🍻</NuxtLink>
+      <NuxtLink class="header__link md:border-r-2" to="/LPGA" >LPGA</NuxtLink>
+      <NuxtLink class="header__link col-span-3" to="https://account.venmo.com/u/ishawshank39" target="_blank" >If you're enjoying this app, buy me a 🍻</NuxtLink>
     </nav>
   </header>
 </template>
@@ -17,8 +17,6 @@ export default defineComponent({})
 
 <style scoped>
 header {
-  height: 40px;
-
   border-bottom: 1px solid;
 }
 
