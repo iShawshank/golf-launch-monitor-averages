66601da3d724d9abd443aed617bb2c6b27e61727
Kevin Shaw <ishawshank39@gmail.com>
2023-03-19 20:41:14 -0600

    Updates

---
+++ _nuxt/error.b8a8166a.js
@@ -0,0 +1,1 @@
+import{a as e,b as o,e as r,o as t}from"./entry.bdcd3152.js";const n=e({props:["error"],mounted(){console.log(this.error)}});function s(c,a,p,_,d,f){return t(),r("div")}const l=o(n,[["render",s]]);export{l as default};
---
+++ _nuxt/tourRedirect.8989a21d.js
@@ -0,0 +1,1 @@
+import{k as a,l as t}from"./entry.bdcd3152.js";const i=a((r,e)=>{if(r.params.tour!=="PGA"&&r.params.tour!=="LPGA")return t()});export{i as default};
